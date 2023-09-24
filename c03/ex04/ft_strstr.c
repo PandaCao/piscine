2995617c9738388e5a654f8bcf6757a66e3b4d4e
Duc Anh Cao <dcao@c2r7s3.42prague.com>
2023-09-24 17:56:43 +0200

    updated sols

--- c03/ex04/ft_strstr.c
+++ c03/ex04/ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: dcao <dcao@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 17:17:53 by dcao              #+#    #+#             */
-/*   Updated: 2023/09/23 21:00:27 by dcao             ###   ########.fr       */
+/*   Updated: 2023/09/24 15:58:38 by dcao             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,29 @@
 
 char	*ft_strstr(char *str, char *to_find)
 {
-	char	*str_ptr;
-	char	*find_ptr;
+	int	i;
+	int	a;
+	int	find_size;
 
-	while (*str != '\0')
+	i = 0;
+	a = 0;
+	find_size = 0;
+	while (to_find[find_size] != '\0')
+		find_size++;
+	if (find_size == 0)
+		return (str);
+	while (str[i] != '\0')
 	{
-		str_ptr = str;
-		find_ptr = to_find;
-		while (*str_ptr == *find_ptr && *find_ptr != '\0')
+		while (to_find[a] == str[i + a])
 		{
-			str_ptr++;
-			find_ptr++;
+			if (a == find_size - 1)
+				return (str + i);
+			a++;
 		}
-		if (*find_ptr == '\0')
-		{
-			return (str);
-		}
-		str++;
+		a = 0;
+		i++;
 	}
-	return (NULL);
+	return (0);
 }
 
 // int	main()
